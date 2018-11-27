111047ed8e984ad0d151fa8138544410df02443c
Elix <elixandre_michael@hotmail.com>
2018-11-27 19:01:41 -0200

    mensagem

--- NEANDER/instruction_UC.vhdl
+++ NEANDER/instruction_UC.vhdl
@@ -101,5 +101,74 @@ architecture comp of STA is
         sel_ula <= "000";
 end architecture;
 
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity NOTs is
+	port(
+        b: in std_logic_vector(3 downto 0);        
+        c_ac :      out std_logic;
+        sel_ula:    out std_logic_vector(2 downto 0);
+        c_PC:       out std_logic;
+        sel_PC:     out std_logic;
+        sel_mux :   out std_logic;
+        c_rem :     out std_logic;
+        c_rdm  :    out std_logic;
+        bloqueios : out std_logic;
+        r_notW :    out std_logic;
+        c_ri :      out std_logic        
+	);
+end NOTs;
+
+architecture comp of NOTs is
+    begin
+        c_PC<=((not(b(1)) or (b(0)) or (b(2))));
+	sel_pc<='1';
+	sel_mux<='0';
+	c_rem<=((not(b(0))) or (b(1)) or (b(2)) or (b(3)));
+	r_notW<='1';
+	c_rdm<=((not(b(1))) or (b(0)) or (b(2)));
+	bloqueios<='1';
+	c_ri<=((not(b(0))) or (not(b(1))) or (b(2)));
+	c_ac<=((b(0)) or (b(1)) or (b(2)) or (b(3)));
+	sel_ula(2)<=((not(b(3)) and not(b(2)) and not(b(1)) and not(b(0))));
+	sel_ula(1)<='0';
+	sel_ula(0)<='0';
+end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity SHIFT is
+	port(
+        b: in std_logic_vector(3 downto 0);        
+        c_ac :      out std_logic;
+        sel_ula:    out std_logic_vector(2 downto 0);
+        c_PC:       out std_logic;
+        sel_PC:     out std_logic;
+        sel_mux :   out std_logic;
+        c_rem :     out std_logic;
+        c_rdm  :    out std_logic;
+        bloqueios : out std_logic;
+        r_notW :    out std_logic;
+        c_ri :      out std_logic        
+	);
+end SHIFT;
+
+architecture comp of SHIFT is
+    begin
+        c_PC<=((not(b(1)) or (b(0)) or (b(2))));
+	sel_pc<='1';
+	sel_mux<='0';
+	c_rem<=((not(b(0))) or (b(1)) or (b(2)) or (b(3)));
+	r_notW<='1';
+	c_rdm<=((not(b(1))) or (b(0)) or (b(2)));
+	bloqueios<='1';
+	c_ri<=((not(b(0))) or (not(b(1))) or (b(2)));
+	c_ac<=((b(0)) or (b(1)) or (b(2)) or (b(3)));
+	sel_ula(2)<=((not(b(3)) and not(b(2)) and not(b(1)) and not(b(0))));
+	sel_ula(1)<='0';
+        sel_ula(0)<=((not(b(3)) and not(b(2)) and not(b(1)) and not(b(0))));
+end architecture;
 
 -- RDM (b(0) xor b(2)) or (not(b(1) and b(0))) or b(3);
